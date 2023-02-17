020925c1cd4a7e8fd179051b114a20cb3fc332a3
Victor <vitek.borodyn@gmail.com>
2023-02-17 20:17:13 +0300

    Fix material-table read property of undefined

--- frontend/src/components/user-table.jsx
+++ frontend/src/components/user-table.jsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import { observer } from "mobx-react-lite";
 import MaterialTable from "material-table";
+import { ThemeProvider, createTheme } from "@mui/material";
 import Context from "../contexts/user-context";
 
 import tableIcons from "./table-icons";
 import { deleteById, getAll, blockById, activateById } from "../http/user-api";
 
 const UserTable = observer(() => {
+  const defaultMaterialTheme = createTheme();
+
   const { user } = useContext(Context);
   const data = user.users;
 
@@ -38,78 +41,87 @@ const UserTable = observer(() => {
   ];
 
   return (
-    <MaterialTable
-      title={null}
-      icons={tableIcons}
-      columns={columns}
-      data={data}
-      options={{
-        selection: true,
-      }}
-      actions={[
-        {
-          tooltip: "Remove all selected users",
-          icon: tableIcons.Delete,
-          onClick: (evt, rows) => {
-            const ids = rows.map((a) => a.id);
+    <div style={{ width: "100%", height: "100%" }}>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+      <ThemeProvider theme={defaultMaterialTheme}>
+        <MaterialTable
+          title={null}
+          icons={tableIcons}
+          columns={columns}
+          data={data}
+          options={{
+            selection: true,
+          }}
+          actions={[
+            {
+              tooltip: "Remove all selected users",
+              icon: tableIcons.Delete,
+              onClick: (evt, rows) => {
+                const ids = rows.map((a) => a.id);
 
-            deleteById(ids);
-            alert("Data synchronization...");
+                deleteById(ids);
+                alert("Data synchronization...");
 
-            getAll().then((syncRows) => {
-              user.setUsers(syncRows);
+                getAll().then((syncRows) => {
+                  user.setUsers(syncRows);
 
-              if (!user.users.map((a) => a.id).includes(user.user.id)) {
-                alert("Sing out...");
-                user.setUser({});
-                user.setIsAuth(false);
-                localStorage.removeItem("token");
-              }
-            });
-          },
-        },
-        {
-          tooltip: "Block all selected users",
-          icon: tableIcons.BlockIcon,
-          onClick: (evt, rows) => {
-            const ids = rows.map((a) => a.id);
+                  if (!user.users.map((a) => a.id).includes(user.user.id)) {
+                    alert("Sing out...");
+                    user.setUser({});
+                    user.setIsAuth(false);
+                    localStorage.removeItem("token");
+                  }
+                });
+              },
+            },
+            {
+              tooltip: "Block all selected users",
+              icon: tableIcons.BlockIcon,
+              onClick: (evt, rows) => {
+                const ids = rows.map((a) => a.id);
 
-            blockById(ids);
-            alert("Data synchronization...");
+                blockById(ids);
+                alert("Data synchronization...");
 
-            getAll().then((syncRows) => {
-              user.setUsers(syncRows);
+                getAll().then((syncRows) => {
+                  user.setUsers(syncRows);
 
-              if (
-                user.users.find(
-                  (element) =>
-                    element.id === user.user.id && element.status === "Blocked",
-                )
-              ) {
-                alert("Sing out...");
-                user.setUser({});
-                user.setIsAuth(false);
-                localStorage.removeItem("token");
-              }
-            });
-          },
-        },
-        {
-          tooltip: "Activate all selected users",
-          icon: tableIcons.CheckCircleIcon,
-          onClick: (evt, rows) => {
-            const ids = rows.map((a) => a.id);
+                  if (
+                    user.users.find(
+                      (element) =>
+                        element.id === user.user.id &&
+                        element.status === "Blocked"
+                    )
+                  ) {
+                    alert("Sing out...");
+                    user.setUser({});
+                    user.setIsAuth(false);
+                    localStorage.removeItem("token");
+                  }
+                });
+              },
+            },
+            {
+              tooltip: "Activate all selected users",
+              icon: tableIcons.CheckCircleIcon,
+              onClick: (evt, rows) => {
+                const ids = rows.map((a) => a.id);
 
-            activateById(ids);
-            alert("Data synchronization...");
+                activateById(ids);
+                alert("Data synchronization...");
 
-            getAll().then((syncRows) => {
-              user.setUsers(syncRows);
-            });
-          },
-        },
-      ]}
-    />
+                getAll().then((syncRows) => {
+                  user.setUsers(syncRows);
+                });
+              },
+            },
+          ]}
+        />
+      </ThemeProvider>
+    </div>
   );
 });
 

ad802ea219b98c7c0e1f967588e2d5c736f2c96c
Victor <vitek.borodyn@gmail.com>
2023-03-21 04:33:35 +0300

    Implement a request limiter

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,12 @@ import sequelize from "./config/database-connection.js";
 import logger from "./config/logger.js";
 import accessLogger from "./middleware/access-logger.js";
 import errorMiddleware from "./middleware/error-handler.js";
+import limiter from "./config/rate-limiter.js";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(limiter);
 app.use(cors());
 app.use(accessLogger);
 app.use(express.json());

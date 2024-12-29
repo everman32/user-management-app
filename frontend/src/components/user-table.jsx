956dda8c57a149f0e3260b3fdb7d2c5c1b06b121
everman32 <vitek.borodyn@gmail.com>
2024-12-29 16:45:36 +0300

    Migrate from material-table to '@mui/material's table component

--- frontend/src/components/user-table.jsx
+++ frontend/src/components/user-table.jsx
@@ -1,132 +1,480 @@
 import React, { useContext } from "react";
 import { observer } from "mobx-react-lite";
-import MaterialTable from "material-table";
-import { ThemeProvider, createTheme } from "@mui/material";
+import PropTypes from "prop-types";
 import Context from "../contexts/user-context";
 
-import tableIcons from "./table-icons";
+import { alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
+import Stack from "@mui/material/Stack";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+import BlockIcon from "@mui/icons-material/Block";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import { visuallyHidden } from "@mui/utils";
+
 import { deleteById, getAll, blockById, activateById } from "../http/user-api";
 
 const UserTable = observer(() => {
-  const defaultMaterialTheme = createTheme();
-
   const { userStore } = useContext(Context);
-  const data = userStore.getUsers();
+  const rows = userStore.getUsers();
 
-  const columns = [
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("");
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelected = rows.map((n) => n.id);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, id) => {
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = (id) => selected.indexOf(id) !== -1;
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const visibleRows = React.useMemo(
+    () =>
+      stableSort(rows, getComparator(order, orderBy)).slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage
+      ),
+    [rows, order, orderBy, page, rowsPerPage]
+  );
+
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  function getComparator(order, orderBy) {
+    return order === "desc"
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
+  // Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
+  // stableSort() brings sort stability to non-modern browsers (notably IE11). If you
+  // only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
+  // with exampleArray.slice().sort(exampleComparator)
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) {
+        return order;
+      }
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+
+  const headCells = [
     {
-      title: "ID",
-      field: "id",
+      id: "id",
+      numeric: true,
+      disablePadding: false,
+      label: "ID",
     },
     {
-      title: "Name",
-      field: "name",
+      id: "name",
+      numeric: false,
+      disablePadding: true,
+      label: "Name",
     },
     {
-      title: "Email",
-      field: "email",
+      id: "email",
+      numeric: false,
+      disablePadding: true,
+      label: "Email",
     },
     {
-      title: "Status",
-      field: "status",
+      id: "status",
+      numeric: false,
+      disablePadding: true,
+      label: "Status",
     },
     {
-      title: "Last logined at",
-      field: "lastLoginedAt",
+      id: "lastLoginedAt",
+      numeric: false,
+      disablePadding: true,
+      label: "Last logined at",
     },
     {
-      title: "Created at",
-      field: "createdAt",
+      id: "createdAt",
+      numeric: false,
+      disablePadding: true,
+      label: "Created at",
     },
   ];
 
+  function EnhancedTableHead(props) {
+    const {
+      onSelectAllClick,
+      order,
+      orderBy,
+      numSelected,
+      rowCount,
+      onRequestSort,
+    } = props;
+    const createSortHandler = (property) => (event) => {
+      onRequestSort(event, property);
+    };
+
+    return (
+      <TableHead>
+        <TableRow>
+          <TableCell padding="checkbox">
+            <Checkbox
+              color="primary"
+              indeterminate={numSelected > 0 && numSelected < rowCount}
+              checked={rowCount > 0 && numSelected === rowCount}
+              onChange={onSelectAllClick}
+              inputProps={{
+                "aria-label": "select all desserts",
+              }}
+            />
+          </TableCell>
+          {headCells.map((headCell) => (
+            <TableCell
+              key={headCell.id}
+              align={headCell.numeric ? "right" : "left"}
+              padding={headCell.disablePadding ? "none" : "normal"}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : "asc"}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <Box component="span" sx={visuallyHidden}>
+                    {order === "desc"
+                      ? "sorted descending"
+                      : "sorted ascending"}
+                  </Box>
+                ) : null}
+              </TableSortLabel>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  }
+
+  EnhancedTableHead.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+  };
+
+  function EnhancedTableToolbar(props) {
+    const { numSelected } = props;
+
+    return (
+      <Toolbar
+        sx={{
+          pl: { sm: 2 },
+          pr: { xs: 1, sm: 1 },
+          ...(numSelected > 0 && {
+            bgcolor: (theme) =>
+              alpha(
+                theme.palette.primary.main,
+                theme.palette.action.activatedOpacity
+              ),
+          }),
+        }}
+      >
+        {numSelected > 0 ? (
+          <Typography
+            sx={{ flex: "1 1 100%" }}
+            color="inherit"
+            variant="subtitle1"
+            component="div"
+          >
+            {numSelected} selected
+          </Typography>
+        ) : (
+          <Typography
+            sx={{ flex: "1 1 100%" }}
+            variant="h6"
+            id="tableTitle"
+            component="div"
+          >
+            Users
+          </Typography>
+        )}
+
+        {numSelected > 0 ? (
+          <Stack direction="row">
+            <Tooltip title="Remove selected users">
+              <IconButton
+                onClick={() => {
+                  deleteById(selected);
+                  alert("Data synchronization...");
+
+                  getAll().then((syncRows) => {
+                    userStore.setUsers(syncRows);
+
+                    if (
+                      !userStore
+                        .getUsers()
+                        .map((a) => a.id)
+                        .includes(userStore.getCurrentUser().id)
+                    ) {
+                      alert("Sing out...");
+                      userStore.setCurrentUser({});
+                      userStore.setIsAuth(false);
+                      localStorage.removeItem("token");
+                    }
+                  });
+                }}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Block selected users">
+              <IconButton
+                onClick={() => {
+                  blockById(selected);
+                  alert("Data synchronization...");
+
+                  getAll().then((syncRows) => {
+                    userStore.setUsers(syncRows);
+
+                    if (
+                      userStore.users.find(
+                        (element) =>
+                          element.id === userStore.getCurrentUser().id &&
+                          element.status === "Blocked"
+                      )
+                    ) {
+                      alert("Sing out...");
+                      userStore.setCurrentUser({});
+                      userStore.setIsAuth(false);
+                      localStorage.removeItem("token");
+                    }
+                  });
+                }}
+              >
+                <BlockIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Activate selected users">
+              <IconButton
+                onClick={() => {
+                  activateById(selected);
+                  alert("Data synchronization...");
+
+                  getAll().then((syncRows) => {
+                    userStore.setUsers(syncRows);
+                  });
+                }}
+              >
+                <CheckCircleIcon />
+              </IconButton>
+            </Tooltip>
+          </Stack>
+        ) : (
+          <Tooltip title="Filter list">
+            <IconButton>
+              <FilterListIcon />
+            </IconButton>
+          </Tooltip>
+        )}
+      </Toolbar>
+    );
+  }
+
+  EnhancedTableToolbar.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+  };
+
   return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
-      <ThemeProvider theme={defaultMaterialTheme}>
-        <MaterialTable
-          title={null}
-          icons={tableIcons}
-          columns={columns}
-          data={data}
-          options={{
-            selection: true,
-          }}
-          actions={[
-            {
-              tooltip: "Remove all selected users",
-              icon: tableIcons.Delete,
-              onClick: (evt, rows) => {
-                const ids = rows.map((a) => a.id);
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={dense ? "small" : "medium"}
+          >
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {visibleRows.map((row, index) => {
+                const isItemSelected = isSelected(row.id);
+                const labelId = `enhanced-table-checkbox-${index}`;
 
-                deleteById(ids);
-                alert("Data synchronization...");
-
-                getAll().then((syncRows) => {
-                  userStore.setUsers(syncRows);
-
-                  if (
-                    !userStore
-                      .getUsers()
-                      .map((a) => a.id)
-                      .includes(userStore.getCurrentUser().id)
-                  ) {
-                    alert("Sing out...");
-                    userStore.setCurrentUser({});
-                    userStore.setIsAuth(false);
-                    localStorage.removeItem("token");
-                  }
-                });
-              },
-            },
-            {
-              tooltip: "Block all selected users",
-              icon: tableIcons.BlockIcon,
-              onClick: (evt, rows) => {
-                const ids = rows.map((a) => a.id);
-
-                blockById(ids);
-                alert("Data synchronization...");
-
-                getAll().then((syncRows) => {
-                  userStore.setUsers(syncRows);
-
-                  if (
-                    userStore.users.find(
-                      (element) =>
-                        element.id === userStore.getCurrentUser().id &&
-                        element.status === "Blocked"
-                    )
-                  ) {
-                    alert("Sing out...");
-                    userStore.setCurrentUser({});
-                    userStore.setIsAuth(false);
-                    localStorage.removeItem("token");
-                  }
-                });
-              },
-            },
-            {
-              tooltip: "Activate all selected users",
-              icon: tableIcons.CheckCircleIcon,
-              onClick: (evt, rows) => {
-                const ids = rows.map((a) => a.id);
-
-                activateById(ids);
-                alert("Data synchronization...");
-
-                getAll().then((syncRows) => {
-                  userStore.setUsers(syncRows);
-                });
-              },
-            },
-          ]}
+                return (
+                  <TableRow
+                    hover
+                    onClick={(event) => handleClick(event, row.id)}
+                    role="checkbox"
+                    aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={row.id}
+                    selected={isItemSelected}
+                    sx={{ cursor: "pointer" }}
+                  >
+                    <TableCell padding="checkbox">
+                      <Checkbox
+                        color="primary"
+                        checked={isItemSelected}
+                        inputProps={{
+                          "aria-labelledby": labelId,
+                        }}
+                      />
+                    </TableCell>
+                    <TableCell align="right">{row.id}</TableCell>
+                    <TableCell
+                      component="th"
+                      id={labelId}
+                      scope="row"
+                      padding="none"
+                    >
+                      {row.name}
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      id={labelId}
+                      scope="row"
+                      padding="none"
+                    >
+                      {row.email}
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      id={labelId}
+                      scope="row"
+                      padding="none"
+                    >
+                      {row.status}
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      id={labelId}
+                      scope="row"
+                      padding="none"
+                    >
+                      {row.lastLoginedAt}
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      id={labelId}
+                      scope="row"
+                      padding="none"
+                    >
+                      {row.createdAt}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (dense ? 33 : 53) * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
         />
-      </ThemeProvider>
-    </div>
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </Box>
   );
 });
 
